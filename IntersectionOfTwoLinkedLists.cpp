e7ba07b7b2eae0c307bec4d7089f947de22bc45b
Kirti Singh <kirti1332000@gmail.com>
2022-06-10 16:35:38 +0530

    Intersection of Two Linked Lists

---
+++ IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+        };
+
+*****************************************************************/
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+    Node* d1 = firstHead;
+    Node* d2 = secondHead;
+    
+    while(d1 != d2) {
+        d1 = d1 == NULL? secondHead:d1->next;
+        d2 = d2 == NULL? firstHead:d2->next;
+    }
+    if(d1==NULL) return -1;
+    return d1->data;
+}
