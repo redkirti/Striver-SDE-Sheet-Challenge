3b9a0798aab648cef9cb9889c0c8fdcc0ea1c1a9
Kirti Singh <kirti1332000@gmail.com>
2022-06-14 13:07:19 +0530

    9. 3 Sum

---
+++ 3sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+    vector<vector<int>> ans;
+    vector<int> temp;
+	sort(arr.begin(), arr.end());
+    for(int i=0; i<n-2; i++){
+        if (i > 0 && arr[i] == arr[i-1])
+            continue;
+        int x = i+1, y = n-1;
+        while(x<y){
+            if(arr[i]+arr[x]+arr[y]==K){
+                ans.push_back({arr[i], arr[x], arr[y]});
+                while(x+1<y && arr[x]==arr[x+1])x++;
+                while(x+1<y && arr[y]==arr[y-1])y--;
+                x++;y--;
+            }
+            else if(arr[i]+arr[x]+arr[y]>K) y--;
+            else x++;
+        }
+    }
+    return ans;
+}
